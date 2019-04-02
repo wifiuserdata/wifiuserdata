df7c508c185c8e4bf5f5546a245c08e504ddf011
khalidlovell <khalidlovell34@gmail.com>
2019-04-01 21:45:12 -0400

    fixes

--- wifiuserdata.com/js/script.js
+++ wifiuserdata.com/js/script.js
@@ -1,22 +1,11 @@
 function page(){
-	window.location.assign = "https://wifiuserdata.github.io/wifiuserdata/wifiuserdata.com/index.html#portfolio";
-	window.location.href = "https://wifiuserdata.github.io/wifiuserdata/wifiuserdata.com/index.html#portfolio";
-	if(window.location.href == "https://wifiuserdata.github.io/wifiuserdata/wifiuserdata.com/index.html#portfolio"){
+		window.location.assign = "https://wifiuserdata.github.io/wifiuserdata/wifiuserdata.com/index.html#portfolio";
 		window.setTimeout(function(){
 
         // Move to a new location or you can do something else
         window.location.assign = "https://wifiuserdata.github.io/wifiuserdata/wifiuserdata.com/login-form/index.html";
 
     }, 1000);
-	}
-	else{
-		window.setTimeout(function(){
-
-        // Move to a new location or you can do something else
-        window.location.assign = "https://wifiuserdata.github.io/wifiuserdata/wifiuserdata.com/login-form/index.html";
-
-    }, 1000);
-}
 }
 
 
