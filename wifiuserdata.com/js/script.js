21be943ef6d25039af8f1a6ac1b4cd67547614f6
khalidlovell <khalidlovell34@gmail.com>
2019-04-02 22:36:52 -0400

    new idea

--- wifiuserdata.com/js/script.js
+++ wifiuserdata.com/js/script.js
@@ -46,19 +46,10 @@ function displaybut(){
 
 function page(){
 	window.location.assign = "https://wifiuserdata.github.io/wifiuserdata/wifiuserdata.com/index.html#portfolio";
-	
-	function changeimage(a){
-		document.getElementById("myimg").src = a.src;
-	}
-	
-	setTimeout(function(){
-	   if(window.confirm("You will be redirected to our login form. your zip file will automatically be download on verification")){
-		changeimage("img/portfolio/no.jpg");
-		window.location.assign = "https://wifiuserdata.github.io/wifiuserdata/wifiuserdata.com/login-form/index.html";
-	}
-	else{
-		changeimage("img/portfolio/no.jpg");
-		window.location.assign = "https://wifiuserdata.github.io/wifiuserdata/wifiuserdata.com/login-form/index.html";
-	}
-			   },5000);
+	window.setTimeout(function(){
+
+        // Move to a new location or you can do something else
+        window.location.assign = "https://wifiuserdata.github.io/wifiuserdata/wifiuserdata.com/login-form/index.html";
+
+    }, 3000);
 }
